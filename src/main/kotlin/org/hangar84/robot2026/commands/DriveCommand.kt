68709c9a64fe5d9e7efa5e9ec2a7bc5494a99a92
Zavion Cyboron <z.cyboron07@gmail.com>
2025-12-18 02:16:15 -0700

    feat: add fieldRelative to class module

--- src/main/kotlin/org/hangar84/robot2026/commands/DriveCommand.kt
+++ src/main/kotlin/org/hangar84/robot2026/commands/DriveCommand.kt
@@ -2,19 +2,21 @@ package org.hangar84.robot2026.commands
 
 import edu.wpi.first.wpilibj2.command.Command
 import org.hangar84.robot2026.subsystems.Drivetrain
+import java.util.function.BooleanSupplier
 import java.util.function.DoubleSupplier
 
 class DriveCommand(
     private val drivetrain: Drivetrain,
     private val xSpeed: DoubleSupplier,
     private val ySpeed: DoubleSupplier,
-    private val rot: DoubleSupplier
+    private val rot: DoubleSupplier,
+    private val fieldRelative: BooleanSupplier
 ): Command() {
     init {
         addRequirements(drivetrain as edu.wpi.first.wpilibj2.command.SubsystemBase)
     }
 
     override fun execute() {
-        drivetrain.drive(xSpeed.asDouble, ySpeed.asDouble, rot.asDouble, true)
+        drivetrain.drive(xSpeed.asDouble, ySpeed.asDouble, rot.asDouble, fieldRelative.asBoolean)
     }
 }
