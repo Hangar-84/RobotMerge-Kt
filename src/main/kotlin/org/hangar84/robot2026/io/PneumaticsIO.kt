3598753265505a664b662fa14347b9cd7d2266ab
Zavion Cyboron <z.cyboron07@gmail.com>
2026-01-23 17:35:32 -0700

    refactor: change the Pneumatics to work with two cylinders

--- src/main/kotlin/org/hangar84/robot2026/io/PneumaticsIO.kt
+++ src/main/kotlin/org/hangar84/robot2026/io/PneumaticsIO.kt
@@ -1,19 +1,19 @@
 package org.hangar84.robot2026.io
 
 interface PneumaticsIO {
-    enum class ActuatorState {EXTEND, RETRACT, NEUTRAL}
+    enum class State { EXTEND, RETRACT, NEUTRAL }
 
     data class Inputs(
-        var compressorEnabled: Boolean = false,
-        var actuatorState: ActuatorState = ActuatorState.NEUTRAL,
-        var extendSolenoidOn: Boolean = false,
-        var retractSolenoidOn: Boolean = false,
-        var pressurePsi: Double? = null,
+        var aState: State = State.NEUTRAL,
+        var bState: State = State.NEUTRAL,
+        var aExtendOn: Boolean = false,
+        var aRetractOn: Boolean = false,
+        var bExtendOn: Boolean = false,
+        var bRetractOn: Boolean = false,
     )
 
     fun updateInputs(inputs: Inputs)
 
-    fun setActuatorState(state: ActuatorState)
-
-    fun setCompressorEnabled(enabled: Boolean) {}
+    fun setA(state: State)
+    fun setB(state: State)
 }
--- src/main/kotlin/org/hangar84/robot2026/subsystems/PneumaticsSubsystem.kt
+++ src/main/kotlin/org/hangar84/robot2026/subsystems/PneumaticsSubsystem.kt
@@ -1,46 +1,71 @@
 package org.hangar84.robot2026.subsystems
 
-import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.hangar84.robot2026.io.PneumaticsIO
-import org.hangar84.robot2026.telemetry.TelemetryRouter
 
-class PneumaticsSubsystem(
-    private val io: PneumaticsIO
-) : SubsystemBase() {
-
-    private val isSim = RobotBase.isSimulation()
+class PneumaticsSubsystem(private val io: PneumaticsIO) : SubsystemBase() {
 
     private val inputs = PneumaticsIO.Inputs()
 
     override fun periodic() {
-
-
         io.updateInputs(inputs)
-
-        TelemetryRouter.pneumatics(inputs.compressorEnabled,
-            inputs.extendSolenoidOn,
-            inputs.retractSolenoidOn,
-            inputs.actuatorState.name,
-            null)
+        // telemetry here using inputs.aState, inputs.bState, inputs.aExtendOn, etc.
     }
 
-    fun extend() = io.setActuatorState(PneumaticsIO.ActuatorState.EXTEND)
-    fun retract() = io.setActuatorState(PneumaticsIO.ActuatorState.RETRACT)
-    fun neutral() = io.setActuatorState(PneumaticsIO.ActuatorState.NEUTRAL)
+    // ----- Actuator A -----
+    fun extendA() = io.setA(PneumaticsIO.State.EXTEND)
+    fun retractA() = io.setA(PneumaticsIO.State.RETRACT)
+    fun neutralA() = io.setA(PneumaticsIO.State.NEUTRAL)
 
-    fun extendCommand(): Command = Commands.runOnce({ extend() }, this)
-    fun retractCommand(): Command = Commands.runOnce({ retract() }, this)
-    fun neutralCommand(): Command = Commands.runOnce({ neutral() }, this)
+    fun toggleA() {
+        val next =
+            if (inputs.aState == PneumaticsIO.State.EXTEND) PneumaticsIO.State.RETRACT
+            else PneumaticsIO.State.EXTEND
+        io.setA(next)
+    }
 
-    fun toggleCommand(): Command = Commands.runOnce({
-        val next = when (inputs.actuatorState) {
-            PneumaticsIO.ActuatorState.EXTEND -> PneumaticsIO.ActuatorState.RETRACT
-            PneumaticsIO.ActuatorState.RETRACT -> PneumaticsIO.ActuatorState.EXTEND
-            PneumaticsIO.ActuatorState.NEUTRAL -> PneumaticsIO.ActuatorState.EXTEND
-        }
-        io.setActuatorState(next)
-    }, this)
+    // ----- Actuator B -----
+    fun extendB() = io.setB(PneumaticsIO.State.EXTEND)
+    fun retractB() = io.setB(PneumaticsIO.State.RETRACT)
+    fun neutralB() = io.setB(PneumaticsIO.State.NEUTRAL)
+
+    fun toggleB() {
+        val next =
+            if (inputs.bState == PneumaticsIO.State.EXTEND) PneumaticsIO.State.RETRACT
+            else PneumaticsIO.State.EXTEND
+        io.setB(next)
+    }
+
+    // ----- Both -----
+    fun setBoth(state: PneumaticsIO.State) {
+        io.setA(state)
+        io.setB(state)
+    }
+
+    fun extendBoth() = setBoth(PneumaticsIO.State.EXTEND)
+    fun retractBoth() = setBoth(PneumaticsIO.State.RETRACT)
+    fun neutralBoth() = setBoth(PneumaticsIO.State.NEUTRAL)
+
+    fun toggleBoth() {
+        val bothExtended =
+            inputs.aState == PneumaticsIO.State.EXTEND &&
+                    inputs.bState == PneumaticsIO.State.EXTEND
+
+        setBoth(if (bothExtended) PneumaticsIO.State.RETRACT else PneumaticsIO.State.EXTEND)
+    }
+
+    // ----- Commands (nice for RobotContainer bindings) -----
+    fun extendACommand(): Command = Commands.runOnce({ extendA() }, this)
+    fun retractACommand(): Command = Commands.runOnce({ retractA() }, this)
+    fun toggleACommand(): Command = Commands.runOnce({ toggleA() }, this)
+
+    fun extendBCommand(): Command = Commands.runOnce({ extendB() }, this)
+    fun retractBCommand(): Command = Commands.runOnce({ retractB() }, this)
+    fun toggleBCommand(): Command = Commands.runOnce({ toggleB() }, this)
+
+    fun extendBothCommand(): Command = Commands.runOnce({ extendBoth() }, this)
+    fun retractBothCommand(): Command = Commands.runOnce({ retractBoth() }, this)
+    fun toggleBothCommand(): Command = Commands.runOnce({ toggleBoth() }, this)
 }
