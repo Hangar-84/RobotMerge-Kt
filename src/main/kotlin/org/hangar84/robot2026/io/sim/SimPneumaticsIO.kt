633539015090e9f0136ece735f308069c55d2dfa
Zavion Cyboron <z.cyboron07@gmail.com>
2026-01-23 17:35:47 -0700

    refactor: change the Pneumatics to work with two cylinders

--- src/main/kotlin/org/hangar84/robot2026/io/sim/SimPneumaticsIO.kt
+++ src/main/kotlin/org/hangar84/robot2026/io/sim/SimPneumaticsIO.kt
@@ -3,22 +3,26 @@ package org.hangar84.robot2026.io.sim
 import org.hangar84.robot2026.io.PneumaticsIO
 
 class SimPneumaticsIO : PneumaticsIO {
-    private var compressorEnabled = true
-    private var state = PneumaticsIO.ActuatorState.NEUTRAL
+
+    private var aState = PneumaticsIO.State.NEUTRAL
+    private var bState = PneumaticsIO.State.NEUTRAL
 
     override fun updateInputs(inputs: PneumaticsIO.Inputs) {
-        inputs.compressorEnabled = compressorEnabled
-        inputs.actuatorState = state
-        inputs.extendSolenoidOn = (state == PneumaticsIO.ActuatorState.EXTEND)
-        inputs.retractSolenoidOn = (state == PneumaticsIO.ActuatorState.RETRACT)
-        inputs.pressurePsi = 110.0
+        inputs.aState = aState
+        inputs.bState = bState
+
+        inputs.aExtendOn = (aState == PneumaticsIO.State.EXTEND)
+        inputs.aRetractOn = (aState == PneumaticsIO.State.RETRACT)
+
+        inputs.bExtendOn = (bState == PneumaticsIO.State.EXTEND)
+        inputs.bRetractOn = (bState == PneumaticsIO.State.RETRACT)
     }
 
-    override fun setActuatorState(newState: PneumaticsIO.ActuatorState) {
-        this.state = newState
+    override fun setA(state: PneumaticsIO.State) {
+        aState = state
     }
 
-    override fun setCompressorEnabled(enabled: Boolean) {
-        compressorEnabled = enabled
+    override fun setB(state: PneumaticsIO.State) {
+        bState = state
     }
 }
